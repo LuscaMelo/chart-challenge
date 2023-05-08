36d7052214e66805ad9048375a21dc1f1a749cc5
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-05-08 11:22:25 -0300

    finished

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -20,13 +20,13 @@ export const Chart = () => {
 
             <div className="bg-theme-300 rounded-3xl p-8 pb-10 mt-6">
                 <h3 className="font-bold text-2xl md:text-3xl text-font">Spending - Last 7 days</h3>
-                <div className="h-[200px] mt-8 md:h-[310px] grid gap-3 md:gap-4 grid-cols-7 pb-8 border-b-2 border-theme-500">
+                <div className="h-[200px] mt-8 md:h-[310px] grid gap-2 md:gap-4 grid-cols-7 pb-8 border-b-2 border-theme-500">
                     {data.map((item, index) => (
                         <div key={index} className="flex flex-col items-center justify-end h-[100%]">
-                            <div className={item.day == day ? `bar relative col-1 bg-theme-blue h-[20%] w-[100%] rounded cursor-pointer` : `bar relative col-1 bg-theme-700 h-[20%] w-[100%] rounded-lg cursor-pointer`}
+                            <div className={item.day == day ? `bar relative col-1 bg-theme-blue h-[20%] w-[100%] rounded cursor-pointer` : `bar relative col-1 bg-theme-700 h-[20%] w-[100%] rounded md:rounded-lg cursor-pointer`}
                                 style={{ height: item.amount * 1.3 + "%" }}
                             >
-                                <div className='hidden absolute top-[-28px] md:top-[-45px] left-[50%] translate-x-[-50%] bg-theme-hover text-white text-[10px] md:text-lg font-bold px-2 py-1 rounded-lg '>{"$" + item.amount}</div>
+                                <div className='hidden absolute top-[-28px] md:top-[-45px] left-[50%] translate-x-[-50%] bg-theme-hover text-white text-[10px] md:text-lg font-bold px-[5px] md:px-3 py-1 rounded '>{"$" + item.amount}</div>
                             </div>
                             <span className="text-sm text-font-light font-semibold mt-2">{item.day}</span>
                         </div>
