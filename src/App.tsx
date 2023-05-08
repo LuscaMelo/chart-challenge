43a89a30e1e5265b580529127984666d476f32eb
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2023-05-08 15:11:03 -0300

    color update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <div className="flex flex-col min-h-[100vh] justify-center items-center bg-theme-500">
+      <h1 className="hidden">Chart Challenge</h1>
       <Chart />
     </div>
   )
